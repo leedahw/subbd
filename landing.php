4a4e69759547c4f23b5b0155afd3e1742f21e6e7
leedahw <71396271+leedahw@users.noreply.github.com>
2020-12-01 22:21:36 -0500

    git rebase commit

--- landing.php
+++ landing.php
@@ -5,17 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="landing page for IMM News Network">
-    <meta name="keywords" content="HTML, PHP, IMM, News, Network, landing page">
+    <meta name="keywords" content="HTML, PHP, subb'd, landing page">
     <meta name="author" content="Alana Dahwoon Lee">
-    <title>SAVERR</title>
+    <title>Subb'd</title>
 </head>
 <body>
 <a href = "homepage.php"><img src = "./imgs/logo.png"/></a>
 <br/>
 
+<form id="login-form">
 <button id= "login-button" class="button">LOGIN</button>
-
+</form>
+<form id="signup-form"> 
 <button id= "signup-button" class="button">SIGNUP</button>
+</form>
+
 
 </body>
 </html>
